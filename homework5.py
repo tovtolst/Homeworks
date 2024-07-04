459ef0c3d62c7328eb630c5f45614a9b0a1dcbbf
tovtolst <tovtolst@yandex.ru>
2024-07-04 23:37:24 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 5, True, 'USSR'
+print (immutable_var)
+#immutable_var[0]=200 - операция невозможна, т.к. в кортеже элементы неизменны.
+mutable_list = [1,2,False,'Russian Federation']
+mutable_list [0] = 5
+mutable_list [1] = 9
+mutable_list [2] = True
+mutable_list [3] = 'President'
+print (mutable_list)
